190ff080c1df64bcdfac7de0f3665c69a2dda6f0
amy-chinlai <amy@actionnetwork.org>
2021-07-13 16:05:34 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :artist, :name
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        return self.artist.name if self.artist
+    end
+
+ 
+
+
+
+
+end
